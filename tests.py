5771a684bb3028ef90c41c6fc6ccadc87203c8f7
yoli <yoli@MacBook-Pro.local>
2022-10-08 11:53:04 +0100

    updated testing

--- tests.py
+++ tests.py
@@ -4,9 +4,7 @@ from calc import calculate
 class TestCalculator(unittest.TestCase):
 
     def test_calculate(self):
+        assert calculate("0+11") == 11
+        assert calculate("11+1-3") == 9
+        assert calculate("11*2") == 22
         assert calculate("12435+34569-12345*10+50") == eval("12435+34569-12345*10+50")
-
-        assert calculate("80*6-4+40*-1") == eval("80*6-4+40*-1")
-
-        assert calculate(5) == 5
-
